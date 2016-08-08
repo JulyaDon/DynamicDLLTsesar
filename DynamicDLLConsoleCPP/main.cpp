e7bfa1e4e25551daa20e16e96f55d5e0d3910fbb
JulyaDon <julya.p.don@gmail.com>
2016-08-08 14:18:27 +0300

    Initial commit

--- DynamicDLLConsoleCPP/main.cpp
+++ DynamicDLLConsoleCPP/main.cpp
@@ -5,21 +5,25 @@
 
 using namespace std;
 
-typedef char* (*PFN_SayHello)();
+//typedef char* (*PFN_SayHello)();
+typedef char* (*PFN_SayHello)(char*);
 
 int main()
 {
 	HINSTANCE hDynamicDLL;
 
-	if ((hDynamicDLL = LoadLibrary(TEXT("DynamicDLL"))) == NULL)
+	//if ((hDynamicDLL = LoadLibrary(TEXT("DynamicDLL"))) == NULL)
+	if ((hDynamicDLL = LoadLibrary(TEXT("CipherTsesarDLL"))) == NULL)
 	{
 		
 	}
 	else
 	{
 		PFN_SayHello pfn_SayHello;
-		pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "SayHello");
-		cout<<(*pfn_SayHello)()<<endl;
+		//pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "SayHello");
+		pfn_SayHello = (PFN_SayHello)GetProcAddress(hDynamicDLL, "mainTsesar");
+		//cout << (*pfn_SayHello)() << endl;
+		cout<<(*pfn_SayHello)("xyz")<<endl;
 	}
 
 
